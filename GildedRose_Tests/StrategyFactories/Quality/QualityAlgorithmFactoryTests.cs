ac878d6eb4f001e24307726da220e8cb85113cf5
Mike Ashton <me@mikeashton.co.uk>
2020-04-07 20:41:08 +0100

    Renamed incorrectly named file

--- GildedRose_Tests/StrategyFactories/Quality/QualityAlgorithmFactoryTests.cs
+++ GildedRose_Tests/StrategyFactories/Quality/QualityAlgorithmFactoryTests.cs
@@ -24,7 +24,7 @@ namespace GildedRose_Tests.StrategyFactories.Quality
     }
     
     [TestClass]
-    public class QualityStrategyFactoryTests
+    public class QualityAlgorithmFactoryTests
     {
         /// <summary>
         /// The type can be created successfully
