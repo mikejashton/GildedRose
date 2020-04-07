57430a2ee92ddb29135da29289cfe28cbd7dbeba
Mike Ashton <me@mikeashton.co.uk>
2020-04-07 21:52:10 +0100

    Added basic implementation of the program entry point

--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using GildedRose.Interfaces;
+using GildedRose.ItemFactories;
+using GildedRose.StrategyFactories;
+using GildedRose.StrategyFactories.Quality;
+using GildedRose.StrategyFactories.Quality.Strategies;
 
 namespace GildedRose
 {
@@ -6,7 +12,67 @@ namespace GildedRose
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Creates a mapping between the quality control strategy and the implementation. This uses
+            // type and not a direct instantiation to prevent to prevent more complicated classes interfering with
+            // each other should this algorithm ultimately be paralleled.
+            var qualityAlgorithmFactory = new QualityAlgorithmFactory( new Dictionary<QualityStrategy, Type>()
+            {
+                { QualityStrategy.Stable, typeof( StableQualityAlgorithm ) },
+                { QualityStrategy.LinearDecrease, typeof( LinearDecreaseAlgorithm ) },
+                { QualityStrategy.LinearIncrease, typeof( LinearIncreaseAlgorithm ) },
+                { QualityStrategy.RapidDecrease, typeof( RapidDecreaseAlgorithm )},
+                //{ QualityStrategy.Stable, typeof( StableQualityAlgorithm ) },
+            });
+            
+            // Creates a mapping between an item of stock and it's corresponding stock management strategy. At the
+            // moment this stock management strategy simply defines how the quality is adjusted, but it could
+            // contain other definitions in future.
+            var itemFactory = new ItemFactory( new Dictionary<string, StockManagementStrategy>()
+            {
+                { "Aged Brie", new StockManagementStrategy( QualityStrategy.LinearIncrease )},
+                { "Backstage passes", new StockManagementStrategy( QualityStrategy.Stable )}, // To do
+                { "Sulfuras", new StockManagementStrategy( QualityStrategy.Stable )},
+                { "Normal Item", new StockManagementStrategy( QualityStrategy.LinearDecrease )},
+                { "Conjured", new StockManagementStrategy( QualityStrategy.RapidDecrease )}
+            });
+            
+            // Create the list of items (this will ultimately be done in a stream)
+            var items = new List<IInventoryItem>()
+            {
+                itemFactory.Create( "Aged Brie", 1, 1 ),
+                itemFactory.Create( "Backstage passes", -1, 2 ),
+                itemFactory.Create( "Backstage passes", 9, 2 ),
+                itemFactory.Create( "Sulfuras", 2, 2 ),
+                itemFactory.Create( "Normal Item", -1, 55 ),
+                itemFactory.Create( "Normal Item", 2, 2 ),
+                //itemFactory.Create( "INVALID ITEM", 2, 2 ),
+                itemFactory.Create( "Conjured", 2, 2 ),
+                itemFactory.Create( "Conjured", -1, 5 ),
+            };
+            
+            // Define a list of quality control steps that must be run every time
+            var qualityControl = new List<IQualityAlgorithm>() { new QualityNeverNegativeAlgorithm() };
+            
+            // And now the object that creates the factory for the item.
+            var pipelineFactory = new QualityPipelineFactory( qualityAlgorithmFactory, qualityControl);
+            
+            items.ForEach(item =>
+            {
+                if (item == null)
+                {
+                    return;
+                }
+                
+                // Decrease the number days by one
+                var shelfLifeMaintainer = item as IShelfLifeMaintenance;
+                shelfLifeMaintainer.SellIn--;
+                
+                var pipeline = pipelineFactory.CreatePipeline( item );
+                foreach (var qualityAlgorithm in pipeline)
+                {
+                    qualityAlgorithm.Run(item, item as IQualityMaintenance );
+                }
+            });
         }
     }
 }
